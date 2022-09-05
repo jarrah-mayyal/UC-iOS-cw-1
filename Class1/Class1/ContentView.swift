6eb7e02d5490fd22262fe08676e33699ce27f626
jarrah-mayyal <46578962+jarrah-mayyal@users.noreply.github.com>
2022-09-05 23:34:01 +0300

    submit

--- Class1/Class1/ContentView.swift
+++ Class1/Class1/ContentView.swift
@@ -9,9 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Image("Back1")
+                .resizable()
+            
+                
+            
+            
+            VStack (spacing: 33) {
+                Text("Hello my name is Jarrah")
+                    .font(.body)
+                    .fontWeight(.medium)
+
+                    
+                    
+                Text("I am 20 years old")
+                    .font(.headline)
+                    .fontWeight(.regular)
+
+                Text("I am learning SWIFTUI with unicode")
+
+                HStack (spacing: 90) {
+                    Image(systemName: "heart.fill")
+                
+                
+                    Image(systemName: "trash.fill")
+
+                    
+                    Image(systemName: "gearshape")
+
+                    
+                }
+
+            }
+            
+        }
+        .ignoresSafeArea()
+        
+        
+    
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
