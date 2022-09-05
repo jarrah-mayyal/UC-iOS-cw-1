161f159b5ed47c78d47428b32378b974de4fa189
jarrah-mayyal <46578962+jarrah-mayyal@users.noreply.github.com>
2022-09-05 19:40:25 +0300

    class work 1

---
+++ Class1/Class1/Class1App.swift
@@ -0,0 +1,17 @@
+//
+//  Class1App.swift
+//  Class1
+//
+//  Created by JARRAH MAYYAL on 05/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Class1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Class1/Class1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Class1
+//
+//  Created by JARRAH MAYYAL on 05/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
